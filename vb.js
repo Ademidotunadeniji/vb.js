2468cdc59edbe0c3ce887720eaab5587ab17dd66
Ademidotunadeniji <ademidotunadeniji32@gmail.com>
2024-12-05 16:44:56 +0100

    first commit

--- vb.js
+++ vb.js
@@ -1,4 +1,4 @@
 const day ='Wednesday'
+console.log(day)
 const position = '3rd'
-console.log('Today is ' + day + ' it is the 3rd ' + ' of the week')
-
+console.log('Today is ' + day + ' it is the 3rd ' + ' of the week ')
